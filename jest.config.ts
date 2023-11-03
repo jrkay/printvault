4a1c6fa4df85f017fc0e57be95ba6edfd06b83c4
jrkay <jrkay@users.noreply.github.com>
2023-11-02 17:30:43 -0700

    Add initial jest test

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   // setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
 }
