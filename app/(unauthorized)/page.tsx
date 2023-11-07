6a2f047b3d9113073b659109fd1606839123d6b2
jrkay <jrkay@users.noreply.github.com>
2023-11-07 01:18:44 -0800

    Update routing as route requires clientside

--- app/(unauthorized)/page.tsx
+++ app/(unauthorized)/page.tsx
@@ -3,7 +3,6 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { redirect } from "next/navigation"
 import { cookies } from "next/headers"
 import { Database } from "@/utils/supabase.ts"
-import { useRouter } from "next/navigation"
 
 async function Page() {
   const supabase = createServerComponentClient<Database>({ cookies })
@@ -11,12 +10,8 @@ async function Page() {
     data: { session },
   } = await supabase.auth.getSession() // get session info
 
-  const router = useRouter()
+  if (session) return redirect(`/dashboard`) // redirect if there is a session
 
-  if (session) {
-    router.push(`/dashboard`)
-    return null
-  }
   return <LoginDisplay />
 }
 
