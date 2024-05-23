6f956335e9bce0d50820e2023cd613fd20979e3d
jrkay <jrkay@users.noreply.github.com>
2024-05-23 14:09:26 -0700

    Rename interface, added explicit type

--- components/ModalComponent.tsx
+++ components/ModalComponent.tsx
@@ -1,35 +1,36 @@
 import React, { ReactNode, useState } from "react"
 import { Modal, Button } from "semantic-ui-react"
 
-type ModalComponent = {
+interface ModalComponentProps {
   triggerText: string
   content: ReactNode
 }
 
-const ModalComponent = ({ triggerText, content }: ModalComponent) => {
-  const [open, setOpen] = useState(false)
+const ModalComponent: React.FC<ModalComponentProps> = ({
+  triggerText,
+  content,
+}) => {
+  const [open, setOpen] = useState<boolean>(false)
 
   const toggleModal = () => setOpen(!open)
 
   return (
-    <>
-      <Modal
-        onClose={toggleModal}
-        onOpen={toggleModal}
-        open={open}
-        trigger={
-          <a onClick={toggleModal} style={{ cursor: "pointer" }}>
-            {triggerText}
-          </a>
-        }
-        className='.bg-000-95'
-      >
-        <Modal.Content className='darkBg'>{content}</Modal.Content>
-        <Modal.Actions className='.bg-000-95'>
-          <Button basic color='violet' content='Close' onClick={toggleModal} />
-        </Modal.Actions>
-      </Modal>
-    </>
+    <Modal
+      onClose={toggleModal}
+      onOpen={toggleModal}
+      open={open}
+      trigger={
+        <span onClick={toggleModal} style={{ cursor: "pointer" }}>
+          {triggerText}
+        </span>
+      }
+      className='bg-000-95'
+    >
+      <Modal.Content className='darkBg'>{content}</Modal.Content>
+      <Modal.Actions className='bg-000-95'>
+        <Button basic color='violet' content='Close' onClick={toggleModal} />
+      </Modal.Actions>
+    </Modal>
   )
 }
 
